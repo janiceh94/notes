68b3c3321a1ca31e07b053cf3f6332957adb2323
janiceh94 <haesung94@gmail.com>
2022-11-02 22:08:07 -0700

    connected Editor to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React from 'react'
+import Editor from "./components/Editor"
+
 function App() {
   return (
     <div className="App">
       <h1> hello </h1>
+      <Editor/>
     </div>
   );
 }
---
+++ src/components/Editor/Editor.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react"
+import ReactMde from "react-mde"
+import Showdown from "showdown"
+
+const Editor = () => {
+
+}
+
+export default Editor
